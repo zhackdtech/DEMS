978b8092ec90fab1b08044ba5131b45359e737b8
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2023-09-18 18:26:19 +0800

    minor modification

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
                         <div class="flex-shrink-0 dropdown">
                             <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle"
                                 data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32"
+                                <img src="" alt="mdo" width="32" height="32"
                                     class="rounded-circle">
                             </a>
                             <ul class="dropdown-menu text-small shadow">
