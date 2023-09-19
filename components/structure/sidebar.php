2bd56ce041a056184d21756360ed73c8623e6191
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2023-09-19 13:51:53 +0800

    coverted elements to  components

---
+++ components/structure/sidebar.php
@@ -0,0 +1,95 @@
+<div class="d-flex flex-column flex-shrink-0 p-4 bg-body-tertiary" style="width: 280px;" id="sidebar">
+    <div class="py-3">
+        <a href="#"
+            class="d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <span class="fs-4 sidenav-brand">
+                <i class="fa-solid fa-network-wired"></i>
+                SYSGO - DEMS
+            </span>
+        </a>
+    </div>
+    <hr class="sidenav-divider">
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item nav-label p-1">
+            MANAGEMENT
+        </li>
+        <li class="nav-item p-1">
+            <a href="#" class="nav-link active" aria-current="page">
+                <i class="nav-icon fa-solid fa-building p-2"></i>
+                &nbsp;Clinics
+            </a>
+        </li>
+        <li class="nav-item p-1">
+            <a href="#" class="nav-link" aria-current="page">
+                <i class="nav-icon fa-solid fa-person p-2"></i>
+                &nbsp;Patients
+            </a>
+        </li>
+        <li class="nav-item p-1">
+            <a href="#" class="nav-link" aria-current="page">
+                <i class="nav-icon fa-solid fa-calendar p-2"></i>
+                &nbsp;Schedule
+            </a>
+        </li>
+        <li class="nav-item nav-label p-1">
+            REPORTING
+        </li>
+        <li class="nav-item p-1">
+            <a href="#" class="nav-link" aria-current="page">
+                <i class="nav-icon fa-solid fa-chart-line p-2"></i>
+                &nbsp;Daily reporting
+            </a>
+        </li>
+        <li class="nav-item p-1">
+            <a href="#" class="nav-link" aria-current="page">
+                <i class="nav-icon fa-solid fa-chart-simple p-2"></i>
+                &nbsp;Sales reporting
+            </a>
+        </li>
+        <li class="nav-item p-1">
+            <a href="#" class="nav-link" aria-current="page">
+                <i class="nav-icon fa-solid fa-chart-column p-2"></i>
+                &nbsp;Patients reporting
+            </a>
+        </li>
+        <li class="nav-item nav-label p-1">
+            TRANSACTIONS
+        </li>
+        <li class="nav-item p-1">
+            <a href="#" class="nav-link" aria-current="page">
+                <i class="nav-icon fa-solid fa-money-bill p-2"></i>
+                &nbsp;Sales history
+            </a>
+        </li>
+        <li class="nav-item p-1">
+            <a href="#" class="nav-link" aria-current="page">
+                <i class="nav-icon fa-solid fa-money-bill-transfer p-2"></i>
+                &nbsp;Expenses
+            </a>
+        </li>
+        <li class="nav-item nav-label p-1">
+            USERS
+        </li>
+        <li class="nav-item p-1">
+            <a href="#" class="nav-link" aria-current="page">
+                <i class="nav-icon fa-solid fa-users p-2"></i>
+                &nbsp;Staffs
+            </a>
+        </li>
+        <li class="nav-item p-1">
+            <a href="#" class="nav-link" aria-current="page">
+                <i class="nav-icon fa-solid fa-user-doctor p-2"></i>
+                &nbsp;Doctors
+            </a>
+        </li>
+        <li class="nav-item nav-label p-1">
+            SYSTEM
+        </li>
+        <li class="nav-item p-1">
+            <a href="#" class="nav-link" aria-current="page">
+                <i class="nav-icon fa-solid fa-clock-rotate-left p-2"></i>
+                &nbsp;Logs
+            </a>
+        </li>
+    </ul>
+</div>
