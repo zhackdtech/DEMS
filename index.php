03d91851203d6cbdd4ae75e3a9467275a4d6d236
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2023-09-19 16:46:34 +0800

    clinic card v1

--- index.php
+++ index.php
@@ -64,7 +64,29 @@
                         </nav>
                     </div>
                     <!-- end breadcrumbs -->
-                    
+                    <div class="row">
+                        <!-- Clinic cards -->
+                        <div class="row">
+                            <div class="col-xl-3 col-lg-6">
+                                <div class="card card-stats mb-4 mb-xl-0">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col">
+                                                <h5 class="card-title text-uppercase text-muted mb-0">PHILIPINES, MANILA</h5>
+                                                <span class="h2 font-weight-bold mb-0">DMS-MANILA</span>
+                                            </div>
+                                            <div class="col-auto">
+                                                <div class="icon icon-shape bg-active text-white shadow">
+                                                    <i class="fas fa-chart-bar"></i>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end clinic cards -->
+                    </div>
                 </div>
                 <div class="tab-pane fade show" id="patients-tab" role="patients-tab" aria-labelledby="patients-tab"
                     tabindex="0">
