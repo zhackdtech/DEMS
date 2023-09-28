c29086fc7721c5716814f24f152946f8a41952d1
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2023-09-29 03:51:42 +0800

    sidebar bug fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
+    $('#sidebarCollapse').on('click', function (e) {
         $('#sidebar').toggleClass('mobile');
+        e.stopPropagation();
     });
     $(document).ready(function () {
         $('a.sidebar-nav-link').on('shown.bs.tab', function (event) {
@@ -13,6 +14,19 @@ $(document).ready(function () {
             $('#sidetabs a[href="' + activeTab + '"]').tab('show');
         }
     });
+    $('body,html').click(function(e){
+        // console.log("body clicked")
+        $('#sidebar').removeClass('mobile');
+        // doesn't work on mobile chrome but works on desktop version
+     });
+     document.onclick = function(e){
+        let menu_icon_box = document.querySelector("#sidebarCollapse");
+        let box = document.querySelector("#sidebar");
+        if (!menu_icon_box.contains(e.target) && !box.contains(e.target) ) {
+            $('#sidebar').removeClass('mobile');
+            // works on mobile and windows chrome
+        }
+    }
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
